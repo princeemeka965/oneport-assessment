3855693270b161563200dfebc0c8b2a189d582d4
princeemeka965 <anyanwue4@gmail.com>
2024-07-01 19:22:19 +0100

    implemented calendar month and year navigation

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -42,8 +42,10 @@ const months: string[] = [
 const Calendar: React.FC<ChildComponentProps> = ({ openDrawer }) => {
   const currentMonth = new Date().getMonth(); // get the current Month
   const currentDay = new Date().getDate(); // get today's date
-  const [year] = useState<number>(2024); // Simulate 2024 as the year to use
-  const [month] = useState<number>(currentMonth); // Set the current
+  const currentYear = new Date().getFullYear(); // get current Year
+
+  const [year, setYear] = useState<number>(currentYear); // Set the current Year
+  const [month, setMonth] = useState<number>(currentMonth); // Set the current
   const [clickedDay, setClickedDay] = useState(0); // get the value of each clicked day
 
   const quotes = generateQuotes(year, month);
@@ -120,6 +122,45 @@ const Calendar: React.FC<ChildComponentProps> = ({ openDrawer }) => {
     return `${months[Number(data)]}`;
   };
 
+  /**
+   *
+   * @param slug
+   * Function to navigate the Calendar using slug params
+   * Here if slug === 'back', this means we are moving backward
+   * Else we are moving up the calendar
+   * NB: Last simulated date is 2024-12-31
+   * This means after 31 december 2024, the Calendar navigates back to 2022
+   */
+  const navigateCalendar = (slug: String) => {
+    switch (slug) {
+      case "back":
+        if (year > 2022) {
+          if (month === 0) {
+            setMonth(11);
+            setYear(year - 1);
+          } else {
+            setMonth(month - 1);
+          }
+        } else {
+          if (month > 0) setMonth(month - 1);
+        }
+        break;
+
+      default:
+        if (year === 2024) {
+          if (month < 11) setMonth(month + 1);
+        } else {
+          if (month === 11) {
+            setMonth(0);
+            setYear(year + 1);
+          } else {
+            setMonth(month + 1);
+          }
+        }
+        break;
+    }
+  };
+
   return (
     <div className="w-full flex flex-col">
       <div className="w-full flex justify-between lg:p-4 md:p-4 py-4 px-1">
@@ -144,10 +185,16 @@ const Calendar: React.FC<ChildComponentProps> = ({ openDrawer }) => {
               </p>
             </span>
             <span className="flex lg:gap-2 md:gap-2 gap-1">
-              <p className="flex flex-col justify-center">
+              <p
+                className="flex flex-col justify-center cursor-pointer"
+                onClick={() => navigateCalendar("back")}
+              >
                 <ChevronLeft />
               </p>
-              <p className="flex flex-col justify-center">
+              <p
+                className="flex flex-col justify-center cursor-pointer"
+                onClick={() => navigateCalendar("forward")}
+              >
                 <ChevronRight />
               </p>
             </span>
