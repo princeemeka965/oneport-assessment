47621cde761301326b45fafd50861e4968f773d4
princeemeka965 <anyanwue4@gmail.com>
2024-07-02 11:02:03 +0100

    function to get time from date

--- src/helpers/timeFormat.ts
+++ src/helpers/timeFormat.ts
@@ -2,3 +2,11 @@ export function convertToISOString(day: number, month: number, year: number) {
   const date = new Date(Date.UTC(year, month, day)); // Month is zero-indexed
   return date.toISOString();
 }
+
+export function convertToLocaleTime(dateISO: any) {
+  let date = new Date(dateISO);
+
+  let options: {} = { hour: "numeric", minute: "numeric", hour12: true };
+  let timeString = date.toLocaleTimeString("en-US", options);
+  return timeString;
+}
